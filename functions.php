2e64da99f32325d6a1cb225a656d79fce5787660
Michael Claybaugh <michael.claybaugh@gmail.com>
2021-10-30 00:39:15 -0400

    removed unwanted boilerplate

--- functions.php
+++ functions.php
@@ -23,14 +23,6 @@ if ( ! function_exists( 'fireplace_setup' ) ) :
      * as indicating support for post thumbnails.
      */
     function fireplace_setup() {
-        /*
-         * Make theme available for translation.
-         * Translations can be filed in the /languages/ directory.
-         * If you're building a theme based on fireplace, use a find and replace
-         * to change 'fireplace' to the name of your theme in all the template files.
-         */
-        load_theme_textdomain( 'fireplace', get_template_directory() . '/languages' );
-
         // Add default posts and comments RSS feed links to head.
         add_theme_support( 'automatic-feed-links' );
 
@@ -70,6 +62,7 @@ if ( ! function_exists( 'fireplace_setup' ) ) :
                 'caption',
                 'style',
                 'script',
+                'navigation-menus',
             )
         );
 
@@ -143,29 +136,20 @@ add_action( 'widgets_init', 'fireplace_widgets_init' );
  */
 function fireplace_scripts() {
     wp_enqueue_style( 'fireplace-style', get_stylesheet_uri(), array(), _S_VERSION );
-    wp_style_add_data( 'fireplace-style', 'rtl', 'replace' );
-
-    wp_enqueue_script( 'fireplace-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );
-    }
 }
 add_action( 'wp_enqueue_scripts', 'fireplace_scripts' );
-
 require get_template_directory() . '/inc/custom-header.php';
 require get_template_directory() . '/inc/template-tags.php';
 require get_template_directory() . '/inc/template-functions.php';
-require get_template_directory() . '/inc/customizer.php';
-if ( defined( 'JETPACK__VERSION' ) ) {
-    require get_template_directory() . '/inc/jetpack.php';
-}
-
 // Post types
 require get_template_directory() . '/post-types/idea.php';
 require get_template_directory() . '/post-types/task.php';
 require get_template_directory() . '/post-types/journal.php';
 
+/**
+ * For private post types, prevent the "publish" status on save.
+ */
+add_filter('wp_insert_post_data', 'fireplace_private_post_types');
 function fireplace_private_post_types($post)
 {
     $private_types = [
@@ -179,4 +163,3 @@ function fireplace_private_post_types($post)
     }
     return $post;
 }
-add_filter('wp_insert_post_data', 'fireplace_private_post_types');
