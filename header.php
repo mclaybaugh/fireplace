d38436c19b9e48c380aafbcfbd0c417793ee6800
Michael Claybaugh <michael.claybaugh@gmail.com>
2021-11-03 20:00:26 -0400

    remove comments, footer content

--- header.php
+++ header.php
@@ -22,20 +22,19 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'fireplace' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#primary">Skip to content</a>
 
-	<header id="masthead" class="site-header">
+	<header class="site-header">
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
+			if (is_front_page() && is_home()) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
 				<?php
 			else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
 				<?php
 			endif;
 			$fireplace_description = get_bloginfo( 'description', 'display' );
@@ -56,4 +55,4 @@
 			);
 			?>
 		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+	</header>
