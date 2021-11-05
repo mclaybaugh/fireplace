429c60ebeb3324e71feada00bef386b2b5c9a588
Michael Claybaugh <michael.claybaugh@gmail.com>
2021-11-04 22:33:53 -0400

    start structure of index and single

--- single.php
+++ single.php
@@ -1,33 +1,25 @@
 <?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package fireplace
- */
-
 get_header();
 ?>
-
-<main id="primary" class="site-main">
-
-	<?php
-	while ( have_posts() ) :
-		the_post();
-
-		get_template_part( 'template-parts/content', get_post_type() );
-
-		the_post_navigation(
-			array(
-				'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'fireplace' ) . '</span> <span class="nav-title">%title</span>',
-				'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'fireplace' ) . '</span> <span class="nav-title">%title</span>',
-			)
-		);
-
-	endwhile; // End of the loop.
-	?>
-
+<main id="primary" class="site-main padding-2">
+	<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	
+	<div class="constrained-width">
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			get_template_part( 'template-parts/content', get_post_type() );
+			?>
+			</div>
+			<?php
+			the_post_navigation(
+				array(
+					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'fireplace' ) . '</span> <span class="nav-title">%title</span>',
+					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'fireplace' ) . '</span> <span class="nav-title">%title</span>',
+				)
+			);
+		endwhile; // End of the loop.
+		?>
 </main><!-- #main -->
 
 <?php
