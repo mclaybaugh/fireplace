510df7e5d4d3b09eb948210e38c322684a28bd03
Michael Claybaugh <michael.claybaugh@gmail.com>
2021-11-08 08:30:02 -0500

    fonts hard-coded in to start

--- header.php
+++ header.php
@@ -18,6 +18,14 @@
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
+	<style>
+	@import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,700;1,400;1,700&family=Montserrat:ital,wght@0,100;0,200;0,400;0,900;1,100;1,200;1,400;1,900&display=swap');
+
+	:root {
+		--body-font: Lori, serif;
+		--heading-font: Montserrat, sans-serif;
+	}
+	</style>
 </head>
 
 <body <?php body_class(); ?>>
