d072fd11a110e469059fd3890996f72fab3eaa05
Michael Claybaugh <michael.claybaugh@gmail.com>
2021-11-19 02:59:36 -0500

    v1.0.3

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'FIREPLACE_VERSION' ) ) {
     // Replace the version number of the theme on each release.
-    define( 'FIREPLACE_VERSION', '1.0.2' );
+    define( 'FIREPLACE_VERSION', '1.0.3' );
 }
 
 if ( ! function_exists( 'fireplace_setup' ) ) :
