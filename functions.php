4aaf302605eb418034827b46d575e69a0fe5ae6f
Michael <michael.claybaugh@gmail.com>
2021-12-02 16:16:07 -0500

    bump version for style updates

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'FIREPLACE_VERSION' ) ) {
     // Replace the version number of the theme on each release.
-    define( 'FIREPLACE_VERSION', '1.1.0' );
+    define( 'FIREPLACE_VERSION', '1.1.1' );
 }
 
 if ( ! function_exists( 'fireplace_setup' ) ) :
